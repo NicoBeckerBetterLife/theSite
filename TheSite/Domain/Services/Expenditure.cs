e982550a031514fbb9ce680ffd392fe02f283e6c
NicoBeckerBetterLife <nico.becker@betterlife.co.za>
2017-02-27 20:24:59 +0200

    installed unity

---
+++ TheSite/Domain/Services/Expenditure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Services
+{
+    public class Expenditure
+    {
+        public void CreateNewExpenditure(Expenditure expenditure)
+        {
+            
+        }
+        public void UpdateExpenditure(Expenditure expenditure)
+        {
+
+        }
+
+        public void removeExpenditure()
+        {
+
+        }
+
+    }
+}
