8542e061880aaca46cb7570ab8a5707383e5ff58
NicoBeckerBetterLife <nico.becker@betterlife.co.za>
2017-02-27 19:21:50 +0200

    Started the App and basic setup

---
+++ TheSite/TheSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheSite.Startup))]
+namespace TheSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
